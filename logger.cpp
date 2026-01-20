e3844c8b3822f5cb192f8ce184fa8467bd0e1516
tdm-kyo <danieleangellotti21@gmail.com>
2026-01-20 13:51:37 +0100

    The exe , the source code and the .txt file

---
+++ logger.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<fstream>
+#include<ctime>
+using namespace std ;
+
+int main()
+{
+	time_t timestamp;
+  	time(&timestamp);
+	ofstream fout("log.txt",ios::app);
+	fout<<ctime(&timestamp);
+	fout<<"\n";
+	cout << "Entrata loggata";;
+	return 0;
+}
